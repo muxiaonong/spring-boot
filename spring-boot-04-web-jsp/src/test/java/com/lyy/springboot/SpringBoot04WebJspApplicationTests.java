3dd350fb094893b101bda27425f28984fe5d7811
839022478 <l>
2019-08-30 11:21:45 +0800

    初始导入

---
+++ spring-boot-04-web-jsp/src/test/java/com/lyy/springboot/SpringBoot04WebJspApplicationTests.java
@@ -0,0 +1,21 @@
+package com.lyy.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Random;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot04WebJspApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println(1);
+		//
+	}
+
+
+}
