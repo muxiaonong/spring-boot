1a1a48cb00226d172c46fe18d97cc14635d1b907
839022478 <l>
2019-09-05 15:47:54 +0800

    初始导入

---
+++ spring-boot-05-crud/src/main/java/com/lyy/springboot/config/MyServerConfig.java
@@ -0,0 +1,62 @@
+package com.lyy.springboot.config;
+
+import com.lyy.springboot.filter.MyFilter;
+import com.lyy.springboot.listener.MyListener;
+import com.lyy.springboot.servlet.MyServlet;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class MyServerConfig {
+
+    //注册三大组件
+    @Bean
+    public ServletRegistrationBean myServlet(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new MyServlet(),"/myServlet");
+        registrationBean.setLoadOnStartup(1);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean myFilter(){
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new MyFilter());
+        registrationBean.setUrlPatterns(Arrays.asList("/hello","/myServlet"));
+        return registrationBean;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean myListener(){
+        ServletListenerRegistrationBean<MyListener> registrationBean = new ServletListenerRegistrationBean<>(new MyListener());
+        return registrationBean;
+    }
+
+
+/*    //配置嵌入式的Servlet容器 EmbeddedServletContainerCustomizer
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+        return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
+
+            //定制嵌入式的Servlet容器相关的规则
+            @Override
+            public void customize(ConfigurableWebServerFactory factory) {
+                factory.setPort(8083);
+            }
+
+
+
+
+
+
+        };
+    }*/
+
+}
