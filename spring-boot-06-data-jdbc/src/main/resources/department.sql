7cc5007e7a9a14a5d55461da8eee86bd6f09950a
839022478 <l>
2019-09-06 15:49:16 +0800

    初始导入

---
+++ spring-boot-06-data-jdbc/src/main/resources/department.sql
@@ -0,0 +1,8 @@
+
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `departmentName` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
