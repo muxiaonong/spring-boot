76695dad1e4ea7c1b61f72b882730bb9b7b66972
839022478 <l>
2019-09-06 13:25:53 +0800

    初始导入

---
+++ spring-boot-05-config/src/main/java/com/lyy/springboot/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.lyy.springboot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        //传入SpringBoot应用的主程序
+        return application.sources(SpringBoot05ConfigApplication.class);
+    }
+
+}
---
+++ spring-boot-05-config/src/main/java/com/lyy/springboot/SpringBoot05ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.lyy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot05ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot05ConfigApplication.class, args);
+    }
+
+}
